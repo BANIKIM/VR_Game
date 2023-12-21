2a8309c3b550c2ed0d29143016afdddecefb3ec9
BANIKIM <72877425+BANIKIM@users.noreply.github.com>
2023-12-21 13:45:41 +0900

    [Feat] Enemy Die 추가

--- Assets/3.Script/Enemy/OnCollisionEnterDeath.cs
+++ Assets/3.Script/Enemy/OnCollisionEnterDeath.cs
@@ -7,4 +7,15 @@ public class OnCollisionEnterDeath : MonoBehaviour
     public string targetTag;
     public Enemy enemy;
 
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag(targetTag))
+        {
+            enemy.Dead(collision.contacts[0].point);
+            //collision.contacts[0].point <- 접촉한 첫번째 Point를 가져온다
+        }
+    }
+
 }
