8ef095c82a97643d0b86787dd3c7fa60a134cc57
BANIKIM <72877425+BANIKIM@users.noreply.github.com>
2023-12-21 13:25:40 +0900

    [Feat] Enemy 추가
    레그돌 적용

---
+++ Assets/3.Script/Enemy/OnCollisionEnterDeath.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollisionEnterDeath : MonoBehaviour
+{
+    public string targetTag;
+    public Enemy enemy;
+
+}
